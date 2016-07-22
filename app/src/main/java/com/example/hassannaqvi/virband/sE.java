3a2d414045e42c54ef6580e977a4e05deed60568
hassan.naqvi <shznaqvi@gmail.com>
2016-07-22 10:38:19 +0500

    Regular Daily Commit July 21 2016

--- app/src/main/java/com/example/hassannaqvi/virband/sE.java
+++ app/src/main/java/com/example/hassannaqvi/virband/sE.java
@@ -2,8 +2,92 @@ package com.example.hassannaqvi.virband;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import butterknife.BindView;
 
 public class sE extends AppCompatActivity {
+
+    private static final String TAG = "SectionE";
+
+    @BindView(R.id.E29)
+    RadioGroup E29;
+    @BindView(R.id.E29_a)
+    RadioButton E29_a;
+    @BindView(R.id.E29_b)
+    RadioButton E29_b;
+    @BindView(R.id.E30)
+    RadioGroup E30;
+    @BindView(R.id.E30_a)
+    RadioButton E30_a;
+    @BindView(R.id.E30_b)
+    RadioButton E30_b;
+    @BindView(R.id.E30_c)
+    RadioButton E30_c;
+    @BindView(R.id.E30_d)
+    RadioButton E30_d;
+    @BindView(R.id.E30_e)
+    RadioButton E30_e;
+    @BindView(R.id.E30_f)
+    RadioButton E30_f;
+    @BindView(R.id.E31)
+    RadioGroup E31;
+    @BindView(R.id.E31_a)
+    RadioButton E31_a;
+    @BindView(R.id.E31_b)
+    RadioButton E31_b;
+    @BindView(R.id.E31_c)
+    RadioButton E31_c;
+    @BindView(R.id.E31_d)
+    RadioButton E31_d;
+    @BindView(R.id.E31_e)
+    RadioButton E31_e;
+    @BindView(R.id.E31_f)
+    RadioButton E31_f;
+    @BindView(R.id.E31_g)
+    RadioButton E31_g;
+    @BindView(R.id.E31_h)
+    RadioButton E31_h;
+    @BindView(R.id.E31_i)
+    RadioButton E31_i;
+    @BindView(R.id.E31_j)
+    RadioButton E31_j;
+    @BindView(R.id.E31_k)
+    RadioButton E31_k;
+    @BindView(R.id.E31_l)
+    RadioButton E31_l;
+    @BindView(R.id.E31_m)
+    RadioButton E31_m;
+    @BindView(R.id.E32)
+    RadioGroup E32;
+    @BindView(R.id.E32_a)
+    RadioButton E32_a;
+    @BindView(R.id.E32_b)
+    RadioButton E32_b;
+    @BindView(R.id.E32_c)
+    RadioButton E32_c;
+    RadioGroup E33;
+    @BindView(R.id.E33_a)
+    RadioButton E33_a;
+    @BindView(R.id.E33_b)
+    RadioButton E33_b;
+    @BindView(R.id.E33_c)
+    RadioButton E33_c;
+    @BindView(R.id.E33_d)
+    RadioButton E33_c;
+    @BindView(R.id.E33_e)
+    RadioButton E33_c;
+    @BindView(R.id.E33_f)
+    RadioButton E33_c;
+    @BindView(R.id.E33_g)
+    RadioButton E33_c;
+    @BindView(R.id.E33_h)
+    RadioButton E33_c;
+
+    
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
