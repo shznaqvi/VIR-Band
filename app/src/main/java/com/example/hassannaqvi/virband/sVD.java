d5d86a47625d4347d8478b6db76b7f04eeab6eb1
hassan.naqvi <shznaqvi@gmail.com>
2016-08-16 14:14:52 +0500

    Regular Daily Commit 16, August 2016

--- app/src/main/java/com/example/hassannaqvi/virband/sVD.java
+++ app/src/main/java/com/example/hassannaqvi/virband/sVD.java
@@ -3,7 +3,8 @@ package com.example.hassannaqvi.virband;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class sVD extends AppCompatActivity {
+public class sVD
+        extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
